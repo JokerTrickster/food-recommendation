7ba1e0b576a6bb80b3aedbe0f602882928e1f25c
myjeong19 <myjeong19@naver.com>
2024-08-16 14:56:39 +0900

    FIX. 구글로그인, 음식 , 제거

--- frontend/src/pages/auth/google.tsx
+++ frontend/src/pages/auth/google.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 
 import { END_POINT_V2 } from '@shared/constants';
+import useAuthStore from '@app/store/user';
 
 export default function Google() {
   const navigate = useNavigate();
@@ -9,6 +10,9 @@ export default function Google() {
   // 현재 url에서 code 부분 추출
   const params = new URLSearchParams(window.location.search);
   const code = params.get('code');
+
+  const setAccessToken = useAuthStore(state => state.setAccessToken);
+  const setUser = useAuthStore(state => state.setUser);
 
   const handleLoginPost = async (code: string) => {
     try {
@@ -19,9 +23,12 @@ export default function Google() {
         },
       });
 
-      const data = response.json();
+      const data = await response.json();
 
-      console.log(data);
+      setAccessToken(data.accessToken);
+      setUser(data.accessToken);
+      localStorage.setItem('accessToken', data.accessToken);
+      localStorage.setItem('refreshToken', data.refreshToken);
     } catch (error) {
       console.log(error);
     }
@@ -34,6 +41,4 @@ export default function Google() {
       console.log('로그인 재시도하세요.');
     }
   }, [code, navigate]);
-
-  return <></>;
 }
