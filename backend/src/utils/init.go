ebaf46ba5f7575c6e701d376121791bc7311d982
JokerTrickster <pkjhj485@gmail.com>
2024-08-20 12:21:08 +0900

    {test} - 로그 파일 테스트

--- backend/src/utils/init.go
+++ backend/src/utils/init.go
@@ -55,7 +55,7 @@ func InitServer() error {
 	}
 	file, err := os.OpenFile("/var/log/myapp.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Failed to open log file, ", err.Error())
 	}
 	log.SetOutput(file)
 	log.Println("Server started")
