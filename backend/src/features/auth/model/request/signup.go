1d7ba1b1f4053c2617086829c52b97d28d24c1eb
JokerTrickster <pkjhj485@gmail.com>
2024-08-29 13:46:45 +0900

    {improvement} - 이메일 가입시 생년월일,성별 필수 기재 \n #81

--- backend/src/features/auth/model/request/signup.go
+++ backend/src/features/auth/model/request/signup.go
@@ -1,7 +1,9 @@
 package request
 
-// 닉네임, 비밀번호, 이메일 정도만 정보를 받는다.
+// 비밀번호, 이메일, 생년월일, 성별 정도만 정보를 받는다.
 type ReqSignup struct {
-	Password string `json:"password"`
-	Email    string `json:"email"`
+	Password string `json:"password" validate:"required,min=6" example:"6글자 이상"`
+	Email    string `json:"email" validate:"required,email"`
+	Sex      string `json:"sex" validate:"required,oneof=male female" example:"male / female"`
+	Birth    string `json:"birth" validate:"required,datetime=2006-01-02" example:"1990-01-01"`
 }
