0352d11945ebd0a127606ec1f1563a4fc2198047
JokerTrickster <pkjhj485@gmail.com>
2024-12-06 00:20:25 +0900

    {improvement} - 프로필 이미지 업로드 개선 \n #236

--- src/utils/aws/s3.go
+++ src/utils/aws/s3.go
@@ -36,13 +36,57 @@ var imgMeta = map[ImgType]imgMetaStruct{
 		bucket:     func() string { return "dev-food-recommendation" },
 		domain:     func() string { return "dev-food-recommendation.s3.ap-northeast-2.amazonaws.com" },
 		path:       "profiles",
-		width:      128,
-		height:     128,
+		width:      512,
+		height:     512,
 		expireTime: 2 * time.Hour,
 	},
 }
 
 func ImageUpload(ctx context.Context, file *multipart.FileHeader, filename string, imgType ImgType) error {
+	meta, ok := imgMeta[imgType]
+	if !ok {
+		return fmt.Errorf("not available meta info for imgType - %v", imgType)
+	}
+	bucket := meta.bucket()
+
+	src, err := file.Open()
+	if err != nil {
+		return fmt.Errorf("fail to open file - %v", err)
+	}
+	defer src.Close()
+
+	img, err := imaging.Decode(src)
+	if err != nil {
+		return fmt.Errorf("fail to load image - %v", err)
+	}
+
+	if meta.width < 1 || meta.height < 1 {
+		if (meta.width < 1 && meta.height < img.Bounds().Size().Y) ||
+			(meta.height < 1 && meta.width < img.Bounds().Size().X) {
+			img = imaging.Resize(img, meta.width, meta.height, imaging.Lanczos)
+		}
+	} else {
+		img = imaging.Fill(img, meta.width, meta.height, imaging.Center, imaging.Lanczos)
+	}
+
+	buf := new(bytes.Buffer)
+	if err := imaging.Encode(buf, img, imaging.PNG, imaging.PNGCompressionLevel(png.BestCompression)); err != nil {
+		return fmt.Errorf("fail to encode png image - %v", err)
+	}
+
+	_, err = awsClientS3Uploader.Upload(ctx, &s3.PutObjectInput{
+		Bucket:      aws.String(bucket),
+		Key:         aws.String(fmt.Sprintf("%s/%s", meta.path, filename)),
+		Body:        buf,
+		ContentType: aws.String("image/png"),
+	})
+	if err != nil {
+		return fmt.Errorf("fail to upload image to s3 - bucket:%s / key:%s/%s", bucket, meta.path, filename)
+	}
+	return nil
+}
+
+func FoodImageUpload(ctx context.Context, file *multipart.FileHeader, filename string, imgType ImgType) error {
 
 	meta, ok := imgMeta[imgType]
 	if !ok {
@@ -105,7 +149,6 @@ func ImageUpload(ctx context.Context, file *multipart.FileHeader, filename strin
 	}
 	return nil
 }
-
 func ImageGetSignedURL(ctx context.Context, fileName string, imgType ImgType) (string, error) {
 	meta, ok := imgMeta[imgType]
 	if !ok {
